501940a1dc8d0152a6955d5b7f77d3bb70cfd3b0
uthdev <adelekegbolahan92@yahoo.com>
2021-12-05 06:31:58 +0100

    add component styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import alanBtn from '@alan-ai/alan-sdk-web';
+import wordsToNumbers from 'words-to-numbers';
+
 import NewsCards from './components/NewsCards/NewsCards';
+import useStyles from './styles';
 
 type IArticles =  {
   i: number;
@@ -10,29 +13,47 @@ type IArticles =  {
   source: {
     name: string;
   }
-  url?: string;
+  url: string;
   urlToImage?: string;
+  activeArticle: number;
 }
 
 
 const alanKey = '437f19a85103a18bf29e5c5cb6b387f62e956eca572e1d8b807a3e2338fdd0dc/stage';
 const App = () => {
+  const classes = useStyles()
   const [newsArticles, setNewsArticles] = useState<IArticles[]>([] as IArticles[])
+  const [activeArticle, setActiveArticle] = useState(-1)
+
   useEffect(() => {
     alanBtn({
       key: alanKey,
-      onCommand: ({ command, articles }: { command: string, articles: IArticles[]}) => {
+      onCommand: ({ command, articles, number }: Record<string, any>) => {
         if(command === 'newHeadlines') {
           console.log(articles)
           setNewsArticles(articles)
+          setActiveArticle(-1)
+        } else if(command === 'highlight') {
+          setActiveArticle((prevActiveArticle) => prevActiveArticle + 1)
+        } else if(command === 'open') {
+          const parsedNumber: number = number.length > 2 ? wordsToNumbers(number, {fuzzy: true }) : number
+          const article = articles[parsedNumber - 1];
+          if(parsedNumber > 20) {
+            alanBtn({key: alanKey}).playText('Please try that again');
+          } else if(article) {
+            window.open(articles[parsedNumber].url, '_blank');
+            alanBtn({key: alanKey}).playText('Opening...')
+          }
         }
       }
     })
-  }, [])
+  },)
   return (
     <div>
-      <h1>Alan AI News Applications</h1>
-      <NewsCards articles={newsArticles}/>
+      <div className={classes.logoContainer}>
+        <img src='https://46ba123xc93a357lc11tqhds-wpengine.netdna-ssl.com/wp-content/uploads/2019/10/alan.jpg' className={classes.alanLogo} alt="alan logo" />
+      </div>
+      <NewsCards articles={newsArticles} activeArticle={activeArticle} />
     </div>
   )
 }
--- src/components/NewsCard/NewCard.tsx
+++ src/components/NewsCard/NewCard.tsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useState, useEffect, createRef, RefObject } from 'react'
 import { Card, CardActionArea, CardActions, CardContent, CardMedia, Button, Typography } from '@material-ui/core'
+import classNames from 'classnames'
 
+import useStyles from './styles'
 export interface ICardProps {
   i: number;
   title?: string;
@@ -9,25 +11,43 @@ export interface ICardProps {
   source: {
     name: string;
   }
-  url?: string;
+  url: string;
   urlToImage?: string;
-}
+  activeArticle: number;
+} 
 
-const NewCard = ({description, publishedAt, source, title, url, urlToImage, i}: ICardProps) => {
+const NewCard = ({description, publishedAt, source, title, url, urlToImage, activeArticle,  i}: ICardProps) => {
+  const classes = useStyles();
+  const [elRefs, setElRefs] = useState([]);
+  const scrollToRef = (ref: RefObject<HTMLElement>) => {
+    const node = ref.current;
+    if(node) {
+      return window.scroll(0, node.offsetTop - 50);
+    }
+  }
+  useEffect(() => {
+    setElRefs((refs) => Array(20).fill(undefined).map((_, j) => refs[j] || createRef()));
+  }, [])
+
+  useEffect(() => {
+    if(i === activeArticle && elRefs[activeArticle]) {
+      scrollToRef(elRefs[activeArticle])
+    }
+  }, [i, activeArticle, elRefs ])
   return (
-    <Card>
-      <CardActionArea>
-        <CardMedia image={urlToImage || 'https://www.google.com/url?sa=i&url=http%3A%2F%2Fwww.thetidenewsonline.com%2F2020%2F07%2F10%2Fmissing-national-light-newspapers-journalist-mathew-onwuasoanya-found%2F&psig=AOvVaw2c8nu6j4cED4CQA3-IZ5PD&ust=1638636803461000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCIjT9fiLyPQCFQAAAAAdAAAAABAD'} />
-        <div>
+    <Card ref={elRefs[i]} className={classNames(classes.card, activeArticle === i ? classes.activeCard : null)}>
+      <CardActionArea href={url} target="_blank">
+        <CardMedia className={classes.media} image={urlToImage || 'http://www.thetidenewsonline.com/wp-content/uploads/2020/07/news-on-go.jpg'} />
+        <div className={classes.details}>
           <Typography variant="body2" color="textSecondary" component="h2">{(new Date(publishedAt)).toDateString()}</Typography>
           <Typography variant="body2" color="textSecondary" component="h2">{source.name}</Typography>
         </div>
-        <Typography gutterBottom variant="h5">{title}</Typography>
+        <Typography className={classes.title} gutterBottom variant="h5">{title}</Typography>
         <CardContent>
           <Typography variant="body2" color="textSecondary" component="p">{description}</Typography>
         </CardContent>
       </CardActionArea>
-      <CardActions>
+      <CardActions className={classes.cardActions}>
         <Button size="small" color="primary">Learn More</Button>
         <Typography variant="h5" color="textSecondary">{i + 1}</Typography>
       </CardActions>
